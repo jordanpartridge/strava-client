86d320f143cde64e1695de1467e5e6367bbf626d
Jordan <jordan.l.partridge@gmail.com>
2024-11-16 19:25:45 -0700

    remove duplicate import

--- src/StravaClient.php
+++ src/StravaClient.php
@@ -11,7 +11,6 @@ use JsonException;
 use Saloon\Exceptions\Request\FatalRequestException;
 use Saloon\Exceptions\Request\RequestException;
 use Saloon\Http\Response;
-use Saloon\Http\Response;
 
 final readonly class StravaClient
 {
