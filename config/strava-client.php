8957dba32aa877897da087e058d6efa941adfe1a
Jordan <jordan.l.partridge@gmail.com>
2024-11-17 11:07:08 -0700

    fix config value

--- config/strava-client.php
+++ config/strava-client.php
@@ -2,5 +2,5 @@
 
 return [
     'base_url' => env('STRAVA_BASE_URL', 'https://www.strava.com/api/v3'),
-    'max_retries' => env('STRAVA_CLIENT_MAX_RETRIES', 3),
+    'max_refresh_attempts' => env('STRAVA_CLIENT_MAX_REFRESH_ATTEMPTS', 3),
 ];
