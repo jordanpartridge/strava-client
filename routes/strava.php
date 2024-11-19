33859054b59d2419d12bd6d82b49baab95197de7
Jordan <jordan.l.partridge@gmail.com>
2024-11-19 12:14:40 -0700

    fix auth routes

--- routes/strava.php
+++ routes/strava.php
@@ -4,7 +4,9 @@ use Illuminate\Support\Facades\Route;
 use JordanPartridge\StravaClient\Http\Controllers\CallBackController;
 use JordanPartridge\StravaClient\Http\Controllers\RedirectController;
 
-Route::prefix('strava')->as('strava:')->group(function () {
-    Route::get('redirect', RedirectController::class)->name('redirect')->middleware('auth:web');
-    Route::get('callback', CallbackController::class)->name('callback');
-});
+Route::prefix('strava')
+    ->as('strava:')
+    ->middleware(['web', config('auth.defaults.guard')])->group(function () {
+        Route::get('redirect', RedirectController::class)->name('redirect');
+        Route::get('callback', CallbackController::class)->name('callback');
+    });
